353da8c1d26a5a93172658e8f7c4273739a8adde
ravisantoshgudimetla <ravisantoshgudimetla@gmail.com>
2018-11-14 23:32:43 -0500

    Make file for running tests

---
+++ hack/verify-gofmt.sh
@@ -0,0 +1,37 @@
+set -o errexit
+set -o nounset
+set -o pipefail
+
+DESCHEDULER_OPERATOR_ROOT=$(dirname "${BASH_SOURCE}")/..
+
+GO_VERSION=($(go version))
+
+if [[ -z $(echo "${GO_VERSION[2]}" | grep -E 'go1.2|go1.3|go1.4|go1.5|go1.6|go1.7|go1.8|go1.9|go1.10') ]]; then
+  echo "Unknown go version '${GO_VERSION}', skipping gofmt."
+  exit 1
+fi
+
+cd "${DESCHEDULER_OPERATOR_ROOT}"
+
+find_files() {
+  find . -not \( \
+      \( \
+        -wholename './output' \
+        -o -wholename './_output' \
+        -o -wholename './release' \
+        -o -wholename './target' \
+        -o -wholename './.git' \
+        -o -wholename '*/third_party/*' \
+        -o -wholename '*/Godeps/*' \
+        -o -wholename '*/vendor/*' \
+      \) -prune \
+    \) -name '*.go'
+}
+
+GOFMT="gofmt -s" 
+bad_files=$(find_files | xargs $GOFMT -l)
+if [[ -n "${bad_files}" ]]; then
+  echo "!!! '$GOFMT' needs to be run on the following files: "
+  echo "${bad_files}"
+  exit 1
+fi
