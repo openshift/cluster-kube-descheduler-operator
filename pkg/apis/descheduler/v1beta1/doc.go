045a4f1cacbe7277b275bab85c8a49d56f3bb9e0
Mike Dame <mdame@redhat.com>
2021-06-08 15:03:43 -0400

    Remove v1beta1 API

--- pkg/apis/descheduler/v1beta1/doc.go
+++
@@ -1,7 +0,0 @@
-// Package v1beta1 contains API Schema definitions for the descheduler v1beta1 API group
-// +k8s:defaulter-gen=TypeMeta
-// +k8s:deepcopy-gen=package,register
-// +groupName=operator.openshift.io
-// +groupGoName=Kubedeschedulers
-// +kubebuilder:validation:Optional
-package v1beta1
