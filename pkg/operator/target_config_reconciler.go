378ee3d97c8494378fde3f03a6d2d189eb0da332
Mike Dame <mdame@redhat.com>
2021-09-07 11:03:27 -0400

    Add hypershift to list of default excluded namespaces

--- pkg/operator/target_config_reconciler.go
+++ pkg/operator/target_config_reconciler.go
@@ -79,7 +79,7 @@ func NewTargetConfigReconciler(
 		klog.ErrorS(err, "error listing namespaces")
 		return nil
 	}
-	excludedNamespaces := []string{"kube-system"}
+	excludedNamespaces := []string{"kube-system", "hypershift"}
 	for _, ns := range allNamespaces.Items {
 		if strings.HasPrefix(ns.Name, "openshift-") {
 			excludedNamespaces = append(excludedNamespaces, ns.Name)
