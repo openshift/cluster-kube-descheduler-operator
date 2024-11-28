23d57e12b25df69c747d67c4946d2d2bd34f3d03
Jan Chaloupka <jchaloup@redhat.com>
2024-11-28 11:14:32 +0100

    Clean root directory

--- pkg/apis/descheduler/v1/doc.go
+++
@@ -1,7 +0,0 @@
-// Package v1 contains API Schema definitions for the descheduler v1 API group
-// +k8s:defaulter-gen=TypeMeta
-// +k8s:deepcopy-gen=package,register
-// +groupName=operator.openshift.io
-// +groupGoName=Kubedeschedulers
-// +kubebuilder:validation:Optional
-package v1
