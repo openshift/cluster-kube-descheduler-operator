47746c07b8cc60fd1b9b40776fd6b50aaa2ffeef
Simone Tiraboschi <stirabos@redhat.com>
2025-04-09 17:19:57 +0200

    [e2e] test softtainter deployment
    Cover also softtainter depployment
    and its cleanup in e2e tests.
    
    Signed-off-by: Simone Tiraboschi <stirabos@redhat.com>

--- test/e2e/operator_test.go
+++ test/e2e/operator_test.go
@@ -2,6 +2,7 @@ package e2e
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"sort"
 	"strings"
@@ -11,11 +12,13 @@ import (
 	descv1 "github.com/openshift/cluster-kube-descheduler-operator/pkg/apis/descheduler/v1"
 	deschclient "github.com/openshift/cluster-kube-descheduler-operator/pkg/generated/clientset/versioned"
 	ssscheme "github.com/openshift/cluster-kube-descheduler-operator/pkg/generated/clientset/versioned/scheme"
+	"github.com/openshift/cluster-kube-descheduler-operator/pkg/softtainter"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	v1 "k8s.io/api/core/v1"
 	apiextclientv1 "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -23,6 +26,7 @@ import (
 	k8sclient "k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/klog/v2"
+	"k8s.io/kubernetes/pkg/util/taints"
 	"k8s.io/utils/clock"
 	utilpointer "k8s.io/utils/pointer"
 
@@ -33,8 +37,22 @@ import (
 	"github.com/openshift/library-go/pkg/operator/resource/resourceread"
 )
 
+const (
+	kubeVirtLabelKey                                   = "kubevirt.io/schedulable"
+	kubeVirtLabelValue                                 = "true"
+	workersLabelSelector                               = "node-role.kubernetes.io/worker="
+	softTainterDeploymentName                          = "softtainter"
+	softTainterServiceAccountName                      = "openshift-descheduler-softtainter"
+	softTainterClusterRoleName                         = "openshift-descheduler-softtainter"
+	softTainterClusterRoleBindingName                  = "openshift-descheduler-softtainter"
+	softTainterClusterMonitoringViewClusterRoleBinding = "openshift-descheduler-softtainter-monitoring"
+	softTainterValidatingAdmissionPolicyName           = "openshift-descheduler-softtainter-vap"
+	softTainterValidatingAdmissionPolicyBindingName    = "openshift-descheduler-softtainter-vap-binding"
+)
+
 var operatorConfigs = map[string]string{
-	"base": "assets/07_descheduler-operator.cr.yaml",
+	"base":                         "assets/07_descheduler-operator.cr.yaml",
+	"devKubeVirtRelieveAndMigrate": "assets/07_descheduler-operator.cr.devKubeVirtRelieveAndMigrate.yaml",
 }
 var operatorConfigsAppliers = map[string]func() error{}
 
@@ -220,6 +238,105 @@ func TestMain(m *testing.M) {
 	klog.Infof("Descheduler (operand) pod running in %v", deschPod.Name)
 
 	os.Exit(m.Run())
+}
+
+func TestSoftTainterDeployment(t *testing.T) {
+	kubeClient := getKubeClientOrDie()
+	ctx, cancelFnc := context.WithCancel(context.TODO())
+	defer cancelFnc()
+
+	// ensure that softtainter additional objects are not there
+	if err := checkSoftTainterObjects(ctx, kubeClient, operatorclient.OperatorNamespace, false); err != nil {
+		t.Fatalf("Unexpected softTainter object: %v", err)
+	}
+	klog.Infof("No one of the softtainter additonal objects is there")
+
+	// label all the nodes to mock a KubeVirt deployment
+	if err := applyKubeVirtNodeLabel(ctx, kubeClient); err != nil {
+		t.Fatalf("Failed applying KubeVirt node label: %v", err)
+	}
+	defer func(ctx context.Context, kubeClient *k8sclient.Clientset) {
+		err := dropKubeVirtNodeLabel(ctx, kubeClient)
+		if err != nil {
+			t.Fatalf("Failed reverting KubeVirt node label: %v", err)
+		}
+	}(ctx, kubeClient)
+
+	// apply devKubeVirtRelieveAndMigrate CR for the operator
+	if err := operatorConfigsAppliers["devKubeVirtRelieveAndMigrate"](); err != nil {
+		t.Fatalf("Unable to apply a CR for Descheduler operator: %v", err)
+	}
+	klog.Infof("Descheduler operator is now configured with devKubeVirtRelieveAndMigrate profile")
+	defer func() {
+		if err := operatorConfigsAppliers["base"](); err != nil {
+			t.Fatalf("Failed restoring base profile: %v", err)
+		}
+	}()
+
+	// wait for softtainter pod to be running
+	stPod, err := waitForPodRunningByNamePrefix(ctx, kubeClient, operatorclient.OperatorNamespace, operatorclient.SoftTainterOperandName, "")
+	if err != nil {
+		t.Fatalf("Unable to wait for the softtainter pod to run")
+	}
+	klog.Infof("SoftTainter pod running in %v", stPod.Name)
+
+	// wait for descheduler pod to be running
+	deschPod, err := waitForPodRunningByNamePrefix(ctx, kubeClient, operatorclient.OperatorNamespace, operatorclient.OperandName, operatorclient.OperandName+"-operator")
+	if err != nil {
+		t.Fatalf("Unable to wait for the Descheduler pod to run")
+	}
+	klog.Infof("Descheduler pod running in %v", deschPod.Name)
+
+	// ensure that all the softtainter additional objects are there
+	if err = checkSoftTainterObjects(ctx, kubeClient, operatorclient.OperatorNamespace, true); err != nil {
+		t.Fatalf("Missing expected softTainter object: %v", err)
+	}
+	klog.Infof("All the softtainter additonal objects got properly created")
+
+	// apply test soft taints
+	if err = applySoftTaints(ctx, kubeClient); err != nil {
+		t.Fatalf("Failed applying softtaints: %v", err)
+	}
+	defer func(ctx context.Context, kubeClient *k8sclient.Clientset) {
+		err := removeSoftTaints(ctx, kubeClient)
+		if err != nil {
+			t.Fatalf("Failed cleaning softtaints: %v", err)
+		}
+	}(ctx, kubeClient)
+
+	// revert to base confing for the operator
+	err = operatorConfigsAppliers["base"]()
+	if err != nil {
+		t.Fatalf("Unable to apply a CR for Descheduler operator: %v", err)
+	}
+	klog.Infof("Descheduler operator is now configured with base profile")
+
+	// wait for softtainter pod to disappear
+	err = waitForPodGoneByNamePrefix(ctx, kubeClient, operatorclient.OperatorNamespace, operatorclient.SoftTainterOperandName, operatorclient.OperandName+"-operator")
+	if err != nil {
+		t.Fatalf("Unable to wait for the softtainter pod to disappear")
+	}
+	klog.Infof("softtainer pod disappeared")
+
+	// ensure that all the softtainter additional objects are gone
+	if err = checkSoftTainterObjects(ctx, kubeClient, operatorclient.OperatorNamespace, false); err != nil {
+		t.Fatalf("Unexpected softTainter object: %v", err)
+	}
+	klog.Infof("No one of the softtainter additonal objects is there")
+
+	// ensure that all the test soft taints got cleaned up
+	softTaint := v1.Taint{Key: softtainter.AppropriatelyUtilizedSoftTaintKey, Value: softtainter.AppropriatelyUtilizedSoftTaintValue, Effect: v1.TaintEffectPreferNoSchedule}
+	nodes, err := kubeClient.CoreV1().Nodes().List(ctx, metav1.ListOptions{LabelSelector: workersLabelSelector})
+	if err != nil {
+		t.Fatalf("Unexpected error fetching nodes: %v", err)
+	}
+	for _, node := range nodes.Items {
+		if taints.TaintExists(node.Spec.Taints, &softTaint) {
+			t.Fatalf("Unexpected leftover softtaint on node %v", node.Name)
+		}
+	}
+	klog.Infof("All the test softtaints got properly cleaned up")
+
 }
 
 func TestDescheduling(t *testing.T) {
@@ -424,4 +541,185 @@ func waitForPodRunningByNamePrefix(ctx context.Context, kubeClient *k8sclient.Cl
 		return nil, err
 	}
 	return expectedPod, nil
+}
+
+func waitForPodGoneByNamePrefix(ctx context.Context, kubeClient *k8sclient.Clientset, namespace, nameprefix, excludedprefix string) error {
+	// Wait until a no pods match nameprefix
+	if err := wait.PollImmediate(5*time.Second, 1*time.Minute, func() (bool, error) {
+		klog.Infof("Listing pods...")
+		podItems, err := kubeClient.CoreV1().Pods(namespace).List(ctx, metav1.ListOptions{})
+		if err != nil {
+			klog.Errorf("Unable to list pods: %v", err)
+			return false, nil
+		}
+		for _, pod := range podItems.Items {
+			if !strings.HasPrefix(pod.Name, nameprefix) || (excludedprefix != "" && strings.HasPrefix(pod.Name, excludedprefix)) {
+				continue
+			}
+			klog.Infof("Found pod: %v, phase: %v, deletionTS: %v\n", pod.Name, pod.Status.Phase, pod.GetDeletionTimestamp())
+			return false, nil
+		}
+		return true, nil
+	}); err != nil {
+		return err
+	}
+	return nil
+}
+
+func checkSoftTainterObjects(ctx context.Context, kubeClient *k8sclient.Clientset, namespace string, expected bool) error {
+	var obj runtime.Object
+	var err error
+
+	obj, err = kubeClient.AppsV1().Deployments(namespace).Get(ctx, softTainterDeploymentName, metav1.GetOptions{})
+	if cerr := checkExpected(expected, obj, err); cerr != nil {
+		return cerr
+	}
+	obj, err = kubeClient.CoreV1().ServiceAccounts(namespace).Get(ctx, softTainterServiceAccountName, metav1.GetOptions{})
+	if cerr := checkExpected(expected, obj, err); cerr != nil {
+		return cerr
+	}
+	obj, err = kubeClient.RbacV1().ClusterRoles().Get(ctx, softTainterClusterRoleName, metav1.GetOptions{})
+	if cerr := checkExpected(expected, obj, err); cerr != nil {
+		return cerr
+	}
+	obj, err = kubeClient.RbacV1().ClusterRoleBindings().Get(ctx, softTainterClusterRoleBindingName, metav1.GetOptions{})
+	if cerr := checkExpected(expected, obj, err); cerr != nil {
+		return cerr
+	}
+	obj, err = kubeClient.RbacV1().ClusterRoleBindings().Get(ctx, softTainterClusterMonitoringViewClusterRoleBinding, metav1.GetOptions{})
+	if cerr := checkExpected(expected, obj, err); cerr != nil {
+		return cerr
+	}
+	obj, err = kubeClient.AdmissionregistrationV1().ValidatingAdmissionPolicies().Get(ctx, softTainterValidatingAdmissionPolicyName, metav1.GetOptions{})
+	if cerr := checkExpected(expected, obj, err); cerr != nil {
+		return cerr
+	}
+	obj, err = kubeClient.AdmissionregistrationV1().ValidatingAdmissionPolicyBindings().Get(ctx, softTainterValidatingAdmissionPolicyBindingName, metav1.GetOptions{})
+	if cerr := checkExpected(expected, obj, err); cerr != nil {
+		return cerr
+	}
+
+	return nil
+
+}
+
+func checkExpected(expected bool, obj runtime.Object, err error) error {
+	if err != nil {
+		if expected {
+			return err
+		} else {
+			if apierrors.IsNotFound(err) {
+				return nil
+			} else {
+				return err
+			}
+		}
+	} else {
+		if expected {
+			return nil
+		} else {
+			metaObj, merr := meta.Accessor(obj)
+			if merr != nil {
+				return fmt.Errorf("cannot get metadata: %w", merr)
+			}
+			return fmt.Errorf("Found unxepected %v %v", obj.GetObjectKind().GroupVersionKind().String(), metaObj.GetName())
+		}
+	}
+}
+
+func applyKubeVirtNodeLabel(ctx context.Context, kubeClient *k8sclient.Clientset) error {
+	nodes, err := kubeClient.CoreV1().Nodes().List(ctx, metav1.ListOptions{LabelSelector: workersLabelSelector})
+	if err != nil {
+		return err
+	}
+	for _, node := range nodes.Items {
+		if node.Labels[kubeVirtLabelKey] != kubeVirtLabelValue {
+			node.Labels[kubeVirtLabelKey] = kubeVirtLabelValue
+			uerr := updateNodeAndRetryOnConflicts(ctx, kubeClient, &node, metav1.UpdateOptions{})
+			if uerr != nil {
+				return uerr
+			}
+		}
+	}
+	return nil
+}
+
+func dropKubeVirtNodeLabel(ctx context.Context, kubeClient *k8sclient.Clientset) error {
+	nodes, err := kubeClient.CoreV1().Nodes().List(ctx, metav1.ListOptions{LabelSelector: workersLabelSelector})
+	if err != nil {
+		return err
+	}
+	for _, node := range nodes.Items {
+		if node.Labels[kubeVirtLabelKey] == kubeVirtLabelValue {
+			delete(node.Labels, kubeVirtLabelKey)
+			uerr := updateNodeAndRetryOnConflicts(ctx, kubeClient, &node, metav1.UpdateOptions{})
+			if uerr != nil {
+				return uerr
+			}
+		}
+	}
+	return nil
+}
+
+func applySoftTaints(ctx context.Context, kubeClient *k8sclient.Clientset) error {
+	softTaint := v1.Taint{Key: softtainter.AppropriatelyUtilizedSoftTaintKey, Value: softtainter.AppropriatelyUtilizedSoftTaintValue, Effect: v1.TaintEffectPreferNoSchedule}
+	nodes, err := kubeClient.CoreV1().Nodes().List(ctx, metav1.ListOptions{LabelSelector: workersLabelSelector})
+	if err != nil {
+		return err
+	}
+	for _, node := range nodes.Items {
+		tNode, updated, terr := taints.AddOrUpdateTaint(&node, &softTaint)
+		if terr != nil {
+			return terr
+		}
+		if updated {
+			uerr := updateNodeAndRetryOnConflicts(ctx, kubeClient, tNode, metav1.UpdateOptions{})
+			if uerr != nil {
+				return uerr
+			}
+		}
+	}
+	return nil
+}
+
+func removeSoftTaints(ctx context.Context, kubeClient *k8sclient.Clientset) error {
+	softTaint := v1.Taint{Key: softtainter.AppropriatelyUtilizedSoftTaintKey, Value: softtainter.AppropriatelyUtilizedSoftTaintValue, Effect: v1.TaintEffectPreferNoSchedule}
+	nodes, err := kubeClient.CoreV1().Nodes().List(ctx, metav1.ListOptions{LabelSelector: workersLabelSelector})
+	if err != nil {
+		return err
+	}
+	for _, node := range nodes.Items {
+		tNode, updated, terr := taints.RemoveTaint(&node, &softTaint)
+		if terr != nil {
+			return terr
+		}
+		if updated {
+			uerr := updateNodeAndRetryOnConflicts(ctx, kubeClient, tNode, metav1.UpdateOptions{})
+			if uerr != nil {
+				return uerr
+			}
+		}
+	}
+	return nil
+}
+
+func updateNodeAndRetryOnConflicts(ctx context.Context, kubeClient *k8sclient.Clientset, node *corev1.Node, opts metav1.UpdateOptions) error {
+	uNode, uerr := kubeClient.CoreV1().Nodes().Update(ctx, node, opts)
+	if uerr != nil {
+		if apierrors.IsConflict(uerr) {
+			if uNode.Name == "" {
+				uNode, uerr = kubeClient.CoreV1().Nodes().Get(ctx, node.Name, metav1.GetOptions{})
+				if uerr != nil {
+					return uerr
+				}
+			}
+			node.Spec.DeepCopyInto(&uNode.Spec)
+			uNode.ObjectMeta.Labels = node.ObjectMeta.Labels
+			uNode.ObjectMeta.Annotations = node.ObjectMeta.Annotations
+			_, err := kubeClient.CoreV1().Nodes().Update(ctx, uNode, opts)
+			return err
+		}
+		return uerr
+	}
+	return nil
 }
