433b2888de0d555c8a4ae37aa3468626b0a86231
Jan Chaloupka <jchaloup@redhat.com>
2025-10-24 13:35:16 +0200

    chore: alter the operator code after the dependencies bump

--- pkg/generated/informers/externalversions/descheduler/v1/kubedescheduler.go
+++ pkg/generated/informers/externalversions/descheduler/v1/kubedescheduler.go
@@ -46,13 +46,25 @@ func NewFilteredKubeDeschedulerInformer(client versioned.Interface, namespace st
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.KubedeschedulersV1().KubeDeschedulers(namespace).List(context.TODO(), options)
+				return client.KubedeschedulersV1().KubeDeschedulers(namespace).List(context.Background(), options)
 			},
 			WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.KubedeschedulersV1().KubeDeschedulers(namespace).Watch(context.TODO(), options)
+				return client.KubedeschedulersV1().KubeDeschedulers(namespace).Watch(context.Background(), options)
+			},
+			ListWithContextFunc: func(ctx context.Context, options metav1.ListOptions) (runtime.Object, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.KubedeschedulersV1().KubeDeschedulers(namespace).List(ctx, options)
+			},
+			WatchFuncWithContext: func(ctx context.Context, options metav1.ListOptions) (watch.Interface, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.KubedeschedulersV1().KubeDeschedulers(namespace).Watch(ctx, options)
 			},
 		},
 		&apisdeschedulerv1.KubeDescheduler{},
