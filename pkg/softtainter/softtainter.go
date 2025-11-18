c2221f069f349f2b6c336644babe1a691618bdfb
Simone Tiraboschi <stirabos@redhat.com>
2025-11-18 15:48:24 +0100

    fix(softtainter): use a custom rate limiter capped to the tick time
    By default controller-runtime uses DefaultTypedControllerRateLimiter
    from k8s.io/client-go/util/workqueue as its rate limiter and this
    can lead to an exponential backoff period up to 1000 seconds in
    the case of consecutive errors.
    Let's configure the controller with a custom rate limiter
    to limit the backoff time to the tick time configured by
    the cluster admin with DeschedulingIntervalSeconds.
    
    Signed-off-by: Simone Tiraboschi <stirabos@redhat.com>

--- pkg/softtainter/softtainter.go
+++ pkg/softtainter/softtainter.go
@@ -10,11 +10,13 @@ import (
 
 	promapi "github.com/prometheus/client_golang/api"
 	"golang.org/x/sync/errgroup"
+	"golang.org/x/time/rate"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/apimachinery/pkg/labels"
 	utilerrors "k8s.io/apimachinery/pkg/util/errors"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/util/workqueue"
 	"k8s.io/kubernetes/pkg/util/taints"
 
 	desv1 "github.com/openshift/cluster-kube-descheduler-operator/pkg/apis/descheduler/v1"
@@ -192,13 +194,8 @@ func (st *softTainter) Reconcile(ctx context.Context, request reconcile.Request)
 	st.resourceNames = getResourceNames(st.args.thresholds)
 
 	err = st.syncSoftTaints(ctx, nodeList)
-	// In case of errors, the exponential backoff mechanism of controller-runtime can easily
-	// exceed the configured tick time.
-	// Never return an error here to honor the fixed tick cadence:
-	// the next execution will still try to process all the nodes
-	// according to fresh data.
 	if err != nil {
-		logger.Error(err, "reconciliation failed")
+		return reconcile.Result{}, err
 	}
 	return reconcile.Result{RequeueAfter: st.resyncPeriod}, nil
 }
@@ -549,17 +546,43 @@ func newNodeUtilizationFactory(promClient promapi.Client, promQuery string) Node
 	return &actualNodeUtilization{promClient: promClient, promQuery: promQuery}
 }
 
+// TickLimitedControllerRateLimiter is a no-arg constructor for a rate limiter for a workqueue.  It has
+// both overall and per-item rate limiting.  The overall is a token bucket and the per-item is exponential
+// up to tickLimit instead of the default value of 1000 seconds
+func TickLimitedControllerRateLimiter[T comparable](tickLimit time.Duration) workqueue.TypedRateLimiter[T] {
+	return workqueue.NewTypedMaxOfRateLimiter(
+		workqueue.NewTypedItemExponentialFailureRateLimiter[T](5*time.Millisecond, tickLimit),
+		// 10 qps, 100 bucket size.  This is only for retry speed, and it's only the overall factor (not per item)
+		&workqueue.TypedBucketRateLimiter[T]{Limiter: rate.NewLimiter(rate.Limit(10), 100)},
+	)
+}
+
 // RegisterReconciler creates a new Reconciler and registers it into manager.
 func RegisterReconciler(mgr manager.Manager, policyConfigFile string) error {
+	des := desv1.KubeDescheduler{}
+	err := mgr.GetAPIReader().Get(context.Background(), client.ObjectKey{
+		Namespace: operatorclient.OperatorNamespace,
+		Name:      operatorclient.OperatorConfigName,
+	}, &des)
+	if err != nil {
+		log.Error(err, "failed reading descheduler operator CR")
+		return err
+	}
+
+	if des.Spec.DeschedulingIntervalSeconds == nil || *des.Spec.DeschedulingIntervalSeconds <= 0 {
+		return fmt.Errorf("descheduler should have an interval set and it should be greater than 0")
+	}
+	resyncPeriod := time.Duration(*des.Spec.DeschedulingIntervalSeconds) * time.Second
 
 	// Create a new controller
 	c, err := controller.New(
 		"nodeclassification-controller",
 		mgr,
 		controller.Options{
+			RateLimiter: TickLimitedControllerRateLimiter[reconcile.Request](resyncPeriod),
 			Reconciler: &softTainter{
 				client:                 mgr.GetClient(),
-				resyncPeriod:           60 * time.Second,
+				resyncPeriod:           resyncPeriod,
 				policyConfigFile:       policyConfigFile,
 				nodeUtilizationFactory: newNodeUtilizationFactory,
 			},
